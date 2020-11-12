4db453604a0a1ffa0f5b2a30f547d5dbcff35ed0
Andre Nüchter <nuechter@ratingen.neofonie.de>
2020-11-12 11:25:05 +0100

    Change structure of src and enable ext for all pages

--- src/background/background.js
+++ src/background/background.js
@@ -1,11 +1,11 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color: '#3aa757' }, () => {
-        console.log("The color is green.");
+        console.log('The color is green.');
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
+                pageUrl: { hostEquals: '<all_urls>' }
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
